3c0d033e187e3319daf0eef3b859b53857435305
Alex <midgard044@gmail.com>
2026-03-18 09:09:11 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,10 +3,13 @@ import os
 
 def move_file(command: str) -> None:
     files = command.split()
-    file_1 = files[1]
-    file_2 = files[2]
+    _, file_1, file_2 = files
+
+    if len(files) != 3:
+        return
+
     if not os.path.basename(file_2):
-        file_2 += file_1
+        file_2 = os.path.join(file_2, file_1)
     new_path = os.path.dirname(file_2)
     if new_path:
         os.makedirs(new_path, exist_ok=True)
